935b8da2ab8b75b0e157ec7937be06773b2c7e6b
Sia <siakaramalegos@gmail.com>
2016-05-26 15:52:31 -0500

    Adds a new dog though render not fully working

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,15 @@ var PUP = PUP || {};
 
 PUP.MainModule = (function(){
 
-  var puppies;
-  var _puppiesURL = "https://ajax-puppies.herokuapp.com/puppies.json"
-  // var _puppyURL = "https://ajax-puppies.herokuapp.com/puppies/:id.json"
+  var puppies, breeds;
+  var _puppiesURL = "https://ajax-puppies.herokuapp.com/puppies.json";
+  var _breedsURL = "https://ajax-puppies.herokuapp.com/breeds.json";
 
   function init(){
     console.log('Initiating main module...');
     _getPuppies();
+    _getBreeds();
+    _listenForNewPuppy();
   }
 
   function _getPuppies(event){
@@ -21,6 +23,17 @@ PUP.MainModule = (function(){
     xhr.addEventListener( "load", function(){
       PUP.MainModule.puppies = JSON.parse(this.responseText);
       _renderPuppies();
+    });
+    xhr.open("GET", _puppiesURL, true);
+    xhr.send();
+  }
+
+  function _getBreeds(event){
+    console.log('Getting breed list.');
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener( "load", function(){
+      PUP.MainModule.breeds = JSON.parse(this.responseText);
+      // _renderBreedsDropdown();
     });
     xhr.open("GET", _puppiesURL, true);
     xhr.send();
@@ -41,21 +54,64 @@ PUP.MainModule = (function(){
     });
 
     puppies.forEach(function(puppy){
-      var $puppyLink = $('<a href="#">adopt</a>')
-        .attr('id', puppy.id)
-        .attr('class', 'adopt-link');
-      var time_stamp = jQuery.timeago(puppy.created_at);
-      var $puppy = $('<li></li>')
-        .append('<strong>' + puppy.name + '</strong>')
-        .append(' (' + puppy.breed.name + '), created ' + time_stamp + ' -- ')
-        .append($puppyLink);
-
-      $('#puppy-list').append($puppy);
+      _renderPuppy(puppy);
     });
 
     // Listeners
     _listenForRefresh();
+    // Where should this go? It won't work for individually added puppies
     _listenForAdopt();
+  }
+
+  function _renderPuppy(puppy){
+    var $puppyLink = $('<a href="#">adopt</a>')
+      .attr('id', puppy.id)
+      .attr('class', 'adopt-link');
+    var time_stamp = jQuery.timeago(puppy.created_at);
+    var $puppy = $('<li></li>')
+      .append('<strong>' + puppy.name + '</strong>')
+      .append(' (' + puppy.breed.name + '), created ' + time_stamp + ' -- ')
+      .append($puppyLink);
+
+    $('#puppy-list').append($puppy);
+  }
+
+
+
+  function _addPuppy(event){
+    event.preventDefault();
+
+    var formData = $(event.target).serializeArray();
+    var puppyData = JSON.stringify({
+      name: formData[0].value,
+      breed_id: formData[1].value
+    });
+    console.log(puppyData);
+
+    $.ajax({
+      url: _puppiesURL,
+      method: 'POST',
+      data: puppyData,
+      contentType: 'application/json',
+      dataType: 'json',
+
+      success: function(json){
+        var puppy = JSON.parse(json);
+        console.log(puppy);
+        _renderPuppy(puppy);
+      },
+
+      error: function(xhr, status, errorThrown){
+        alert( "Sorry, there was a problem!" );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+      },
+
+      complete: function( xhr, status ) {
+        console.log( "The request is complete!" );
+      }
+    });
   }
 
   function _adoptPuppy(event){
@@ -81,6 +137,10 @@ PUP.MainModule = (function(){
 
   function _listenForAdopt(){
     $('.adopt-link').click(_adoptPuppy);
+  }
+
+  function _listenForNewPuppy(){
+    $('form#new-puppy').submit(_addPuppy);
   }
 
   return {
