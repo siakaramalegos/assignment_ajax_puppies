8bb87e7bcefb78e307f5dc44269aa0c217879de7
Sia <siakaramalegos@gmail.com>
2016-05-26 14:26:58 -0500

    Sorts puppies and adopts though re-rendering not working

--- scripts/main.js
+++ scripts/main.js
@@ -13,21 +13,37 @@ PUP.MainModule = (function(){
     _getPuppies();
   }
 
-  function _getPuppies(){
-    console.log('Rendering puppy list.');
+  function _getPuppies(event){
+    if (event) { event.preventDefault(); }
+
+    console.log('Getting puppy list.');
     var xhr = new XMLHttpRequest();
     xhr.addEventListener( "load", function(){
       PUP.MainModule.puppies = JSON.parse(this.responseText);
       _renderPuppies();
-      console.log( PUP.MainModule.puppies );
     });
     xhr.open("GET", _puppiesURL, true);
     xhr.send();
   }
 
   function _renderPuppies(){
-    PUP.MainModule.puppies.forEach(function(puppy){
-      var $puppyLink = $('<a>adopt</a>');
+    // Clear current list
+    $('#puppy-list').html('');
+
+    var puppies = PUP.MainModule.puppies.sort(function(a, b){
+      if (a.created_at > b.created_at) {
+        return -1
+      } else if (a.created_at < b.created_at) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    puppies.forEach(function(puppy){
+      var $puppyLink = $('<a href="#">adopt</a>')
+        .attr('id', puppy.id)
+        .attr('class', 'adopt-link');
       var time_stamp = jQuery.timeago(puppy.created_at);
       var $puppy = $('<li></li>')
         .append('<strong>' + puppy.name + '</strong>')
@@ -36,6 +52,35 @@ PUP.MainModule = (function(){
 
       $('#puppy-list').append($puppy);
     });
+
+    // Listeners
+    _listenForRefresh();
+    _listenForAdopt();
+  }
+
+  function _adoptPuppy(event){
+    event.preventDefault();
+
+    console.log('Adopting puppy #' + event.target.id);
+    var url = "https://ajax-puppies.herokuapp.com/puppies/" + event.target.id + ".json";
+
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener( "load", function(){
+      _getPuppies;
+    });
+    xhr.open("DELETE", url, true);
+    xhr.send();
+  }
+
+
+
+  // Listeners
+  function _listenForRefresh(){
+    $('#refresh').click(_getPuppies);
+  }
+
+  function _listenForAdopt(){
+    $('.adopt-link').click(_adoptPuppy);
   }
 
   return {
